14b3bf8149e4f95b5ddd8b4f8516a6ae129c8476
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-19 13:49:51 -0400

    initial commit

--- src/leapyearcalc.java
+++ src/leapyearcalc.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class leapyearcalc {
+public class leapyearcalc {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            return div4Check(year);
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean div4Check(int year) {
+        if (year % 4 == 0) {
+            return div100Check(year);
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean div100Check(int year) {
+        if (year % 100 == 0) {
+            return div400Check(year);
+        } else {
+            return true;
+        }
+    }
+
+    private static boolean div400Check(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
